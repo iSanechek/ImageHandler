125cce8a1478e8ae0044cbf2f42fccd393fc672f
isanechek <neizvestniytip@gmail.com>
2020-07-08 23:42:22 +0400

    some change splash screen

--- app/src/main/java/com/isanechek/imagehandler/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/isanechek/imagehandler/ui/splash/SplashFragment.kt
@@ -1,13 +1,16 @@
 package com.isanechek.imagehandler.ui.splash
 
 import android.os.Bundle
+import com.isanechek.imagehandler.BuildConfig
 import com.isanechek.imagehandler._layout
 import com.isanechek.imagehandler.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.splash_fragment_layout.*
 
 class SplashFragment : BaseFragment(_layout.splash_fragment_layout) {
 
     override fun bindUi(savedInstanceState: Bundle?) {
-
+        val text = "alpha version\nверсия.${BuildConfig.VERSION_NAME}"
+        ss_build_version.text = text
     }
 
 }
